b02294855be4bcc7ff58e00085539f4f984cbb44
Divyansh Jha <jhadivyansh29@gmail.com>
2024-02-12 23:50:58 +0530

    All Files

---
+++ script.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDk7KmMFLNOgHBJ1wAT_tYNYannPP2BLNI",
+    authDomain: "loginlab-8dd00.firebaseapp.com",
+    databaseURL: "https://loginlab-8dd00-default-rtdb.firebaseio.com",
+    projectId: "loginlab-8dd00",
+    storageBucket: "loginlab-8dd00.appspot.com",
+    messagingSenderId: "701168362976",
+    appId: "1:701168362976:web:5b0552036db092f6687785"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
